5de7573a4ed339571cc6f56fe84f50e2c82ebb68
Victor Gabriel <victorgpjm0@gmail.com>
2026-09-12 13:15:24 -0300

    Adicionando dados mockados

---
+++ database/dados_mockados.sql
@@ -0,0 +1,5 @@
+INSERT INTO Operador (nome, login, senha, turno) 
+VALUES ('Carlos Silva', 'carlos.portaria', '1234', 'Manhã');
+
+INSERT INTO Usuario (cpf, cargo, nome) 
+VALUES ('11122233344', 'Professor', 'Ana Paula Souza');
